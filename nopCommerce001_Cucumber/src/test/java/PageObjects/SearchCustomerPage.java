c9502b7627415e9fa722329278e4ac2ade5dc0a2
ramesh19831 <hanimicse@gmail.com>
2021-05-12 11:52:49 +0530

    Added Search customer page

---
+++ nopCommerce001_Cucumber/src/test/java/PageObjects/SearchCustomerPage.java
@@ -0,0 +1,14 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class SearchCustomerPage {
+	
+	WebDriver driver;
+	
+	public SearchCustomerPage (WebDriver driver) {
+		this.driver = driver;
+	}
+	
+
+}
